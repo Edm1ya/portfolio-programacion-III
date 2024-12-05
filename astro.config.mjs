66cacafc8377765369277534a3f0279c03835c08
Edward Minaya <ed.miya.14@gmail.com>
2024-12-04 23:31:38 -0400

    astro deply done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  site: 'https://Edm1ya.github.io',
+  base: '/portfolio-programacion-III',
 });
